d727d6949e68e552f1f1b4c43e3a76d8693425b0
vlk2022 <volodymyrkostiuk2021@gmail.com>
2024-02-16 00:52:42 +0200

    add angular projects

--- src/components/portfolioComponents/AngularPortfolioComponent/AngularPortfolioComponent.js
+++ src/components/portfolioComponents/AngularPortfolioComponent/AngularPortfolioComponent.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
+import angularData from '../../../data/projects/angularData';
 import classes from "./AngularPortfolioComponent.module.css";
+import {SinglePortfolioComponents} from "../SinglePortfolioComponents/SinglePortfolioComponents";
 
 
 const AngularPortfolioComponent = () => {
     return (
         <main className={`${classes.wrap} width`}>
-            AngularPortfolioComponent
+            {angularData.map(obj => <SinglePortfolioComponents key={obj.id} obj={obj}/>)}
         </main>
     );
 };
--- src/data/projects/angularData.js
+++ src/data/projects/angularData.js
@@ -1,3 +1,23 @@
-const angularData = [];
+const angularData = [
+    {
+        id: 'ang1',
+        name: 'Search Movies',
+        technologies: 'Angular, Typescript, rxjx, angular-fontawesome, angular-forms',
+        image: '/projectFotos/searchAngular.jpg',
+        link: '#',
+        video: '/projectVideos/searchAngular.mp4',
+        code: 'https://github.com/VLK2021/searchMoviesAngular/tree/master'
+    },
+    {
+        id: 'ang2',
+        name: 'Password',
+        technologies: 'Angular, Typescript, rxjx',
+        image: '/projectFotos/angularPass.jpg',
+        link: 'https://usense-test-angular.vercel.app/home',
+        video: '/projectVideos/passAngular.mp4',
+        code: 'https://github.com/VLK2021/usense-test-angular/tree/master'
+    }
+
+];
 
 export default angularData;
