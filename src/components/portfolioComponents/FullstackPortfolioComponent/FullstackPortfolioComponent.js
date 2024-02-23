b282d996f7fcfeea005bc737d7d2817b1450bd1b
vlk2022 <volodymyrkostiuk2021@gmail.com>
2024-02-24 00:01:32 +0200

    add fullstack projects

--- src/components/portfolioComponents/FullstackPortfolioComponent/FullstackPortfolioComponent.js
+++ src/components/portfolioComponents/FullstackPortfolioComponent/FullstackPortfolioComponent.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import classes from "./FullstackPortfolioComponent.module.css";
+import fullstackData from "../../../data/projects/fullstackData";
+import {SinglePortfolioComponents} from "../SinglePortfolioComponents/SinglePortfolioComponents";
 
 
 const FullstackPortfolioComponent = () => {
     return (
-        <main className={`${classes.wrap}`}>
-            FullstackPortfolioComponent
+        <main className={`${classes.wrap} width`}>
+            {fullstackData.map(obj => <SinglePortfolioComponents key={obj.id} obj={obj}/>)}
         </main>
     );
 };
--- src/data/projects/fullstackData.js
+++ src/data/projects/fullstackData.js
@@ -1,3 +1,13 @@
-const fullstackData = [];
+const fullstackData = [
+    {
+        id: 'full1',
+        name: 'CRUD',
+        technologies: 'React, React Hook Form, Node, mysql2, express, Typescript, typeorm',
+        image: '/projectFotos/crud.jpg',
+        link: '#',
+        video: '/projectVideos/crud.mp4',
+        code: 'https://github.com/VLK2021/crud-react-node'
+    },
+];
 
 export default fullstackData;
